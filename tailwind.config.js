4d693b5549ea69a05c4e703785dcf3d37ebc52d1
Amasias-Rodriguez <rodamasias01@gmail.com>
2023-10-26 22:10:22 -0600

    files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+
+        'Hanken-Grotesk': ['Hanken-Grotesk', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
